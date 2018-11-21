9ca14b89ad99c2abd65d34e0e0d78ac09164544b
georgikrastev <georgi.krystev89@gmail.com>
2018-11-21 23:24:22 +0200

    Add latest jQuery and update site.js

--- scripts/site.js
+++ scripts/site.js
@@ -1,4 +1,3 @@
-
 /**
  * This script wrapped in a Immediately-Invoked Function Expression (IIFE) to
  * prevent variables from leaking onto the global scope. For more information
@@ -7,20 +6,22 @@
  */
 
 (function() {
-  'use strict';
+	var $win = $(window);
+	var $doc = $(document);
 
-  // Load all images via Squarespace's Responsive ImageLoader
-  function loadAllImages() {
-    var images = document.querySelectorAll('img[data-src]' );
-    for (var i = 0; i < images.length; i++) {
-      ImageLoader.load(images[i], {load: true});
-    }
-  }
+	// Load all images via Squarespace's Responsive ImageLoader
+	function loadAllImages() {
+		var images = document.querySelectorAll('img[data-src]' );
+		for (var i = 0; i < images.length; i++) {
+			ImageLoader.load(images[i], {load: true});
+		}
+	}
 
-  // The event subscription that loads images when the page is ready
-  document.addEventListener('DOMContentLoaded', loadAllImages);
+	// The event subscription that loads images when the page is ready
+	document.addEventListener('DOMContentLoaded', loadAllImages);
 
-  // The event subscription that reloads images on resize
-  window.addEventListener('resize', loadAllImages);
+	// The event subscription that reloads images on resize
+	window.addEventListener('resize', loadAllImages);
 
+	// Your custom JS goes here
 }());
